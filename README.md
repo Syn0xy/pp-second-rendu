e5686240855b2af33081a0cd1fb084f762c778c2
Synoxy <sinoxap@gmail.com>
2026-03-27 17:50:06 +0100

    Mise en place du dépôt (le code cuda n'est pas fini)

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # pp-second-rendu
+
+## Temporaire
+
+Compte-rendu de TP correspondant à l'exercice suivant : 
+
+> "Exercice 3 – Calcul de fractales sur GPU" du TP4.
+
+Le compte-rendu devra contenir :
+- votre code source (sans fichier objet, et sans programme compilé), en nommant clairement vos différentes versions ;
+- un rapport de quelques pages (au format pdf) présentant votre travail.
+
+La clarté de votre présentation, et les analyses de vos tests de performance auront une part importante dans la notation du compte-rendu.
---
+++ src/CUDA_common.h
@@ -0,0 +1,44 @@
+#include <cuda.h>
+#include <stdio.h>
+
+
+/** 
+ * CUDA error handling and debugging 
+ */
+
+#ifdef CUDA_DEBUG
+#define CUDA_SYNC_ERROR() {						\
+    cudaError_t sync_error;						\
+    cudaDeviceSynchronize();						\
+    sync_error = cudaGetLastError();					\
+    if(sync_error != cudaSuccess) {					\
+      fprintf(stderr, "[CUDA SYNC ERROR at %s:%d -> %s]\n",		\
+	      __FILE__ , __LINE__, cudaGetErrorString(sync_error));	\
+      exit(EXIT_FAILURE);						\
+    }									\
+  }
+#else /* #ifdef CUDA_DEBUG */
+#define CUDA_SYNC_ERROR()
+#endif /* #ifdef CUDA_DEBUG */
+
+#define CUDA_ERROR(cuda_call) {					\
+    cudaError_t error = cuda_call;				\
+    if(error != cudaSuccess){					\
+      fprintf(stderr, "[CUDA ERROR at %s:%d -> %s]\n",		\
+	      __FILE__ , __LINE__, cudaGetErrorString(error));	\
+      exit(EXIT_FAILURE);					\
+    }								\
+    CUDA_SYNC_ERROR();						\
+  }
+
+
+
+/**
+ * Returns the integer quotient greater or equal to "a/b". 
+ * From: CUDA SDK 4.1 
+ */
+
+static int iDivUp(int a, int b){
+  return ((a % b != 0) ? (a / b + 1) : (a / b));
+}
+
